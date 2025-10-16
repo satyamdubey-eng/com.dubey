7241cd45c91208238a392727b36213c4379dbb4e
satyam <satyam.dubey@fleekitsolutions.com>
2025-10-16 10:49:10 +0530

    this is it

--- src/test/java/Package2/Test2.java
+++ src/test/java/Package2/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("This is Lucknow");
 		System.out.println("This is agra");
+		System.out.println("this is Bareily");
 	}
 
 }
